2ccd549be95a02a4f7806757791e6a8a7a7ba074
Richard Huang <flyerhzm@gmail.com>
2021-08-21 09:46:46 +0800

    add ReplaceWithAction

--- test/action.spec.js
+++ test/action.spec.js
@@ -1,16 +1,15 @@
 const espree = require("espree");
-require("../lib/ast-node-ext");
 
-const { Action, ReplaceAction } = require("../lib/action");
+const Instance = require("../lib/instance");
+const { Action, ReplaceAction, ReplaceWithAction } = require("../lib/action");
 
 const parse = (code) => espree.parse(code, { ecmaVersion: 'latest', loc: true, sourceFile: 'code.js' }).body[0];
 
 describe("action", () => {
-  let action;
-  beforeAll(() => {
-    const node = parse("class FooBar {}");
-    action = new Action({ currentNode: node }, '{{id}}');
-  });
+  const node = parse("class FooBar {}");
+  const instance = new Instance({}, '', function() {})
+  instance.currentNode = node
+  const action = new Action(instance, '{{id}}');
 
   it("gets rewrittenSource", function() {
     expect(action.rewrittenSource()).toBe("FooBar");
@@ -18,11 +17,10 @@ describe("action", () => {
 });
 
 describe("ReplaceAction", () => {
-  let action;
-  beforeAll(() => {
-    const node = parse("class FooBar {}");
-    action = new ReplaceAction({ currentNode: node }, 'id', { with: 'Synvert' });
-  });
+  const node = parse("class FooBar {}");
+  const instance = new Instance({}, '', function() {})
+  instance.currentNode = node
+  const action = new ReplaceAction(instance, 'id', { with: 'Synvert' });
 
   it("gets beginPos", function() {
     expect(action.beginPos()).toBe(6);
@@ -34,5 +32,24 @@ describe("ReplaceAction", () => {
 
   it("gets rewrittenCode", function() {
     expect(action.rewrittenCode()).toBe("Synvert");
+  });
+});
+
+describe("ReplaceWithAction", () => {
+  const node = parse("!!foobar");
+  const instance = new Instance({}, '', function() {})
+  instance.currentNode = node
+  const action = new ReplaceWithAction({ currentNode: node }, 'Boolean({{expression.argument.argument}})');
+
+  it("gets beginPos", function() {
+    expect(action.beginPos()).toBe(0);
+  });
+
+  it("gets endPos", function() {
+    expect(action.endPos()).toBe(8);
+  });
+
+  it("gets rewrittenCode", function() {
+    expect(action.rewrittenCode()).toBe("Boolean(foobar)");
   });
 });
