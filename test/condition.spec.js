08931cfd16c48571a2340bd6901e9b4c29a25364
Richard Huang <flyerhzm@gmail.com>
2021-08-25 19:12:32 +0800

    add ifExistNode dsl

---
+++ test/condition.spec.js
@@ -0,0 +1,32 @@
+const espree = require("espree");
+
+const { IfExistCondition } = require("../lib/condition");
+const Instance = require("../lib/instance");
+
+describe("IfExistsCondition", () => {
+  const source = `
+    'use strict'
+
+    this.foobar
+  `
+  const node = espree.parse(source, { ecmaVersion: 'latest', loc: true, sourceFile: 'code.js' });
+  const instance = new Instance({}, '', function() {})
+
+  describe("process", () => {
+    beforeAll(() => {
+      instance.currentNode = node
+    })
+
+    test("does not call function if no matching node", () => {
+      let run = false
+      new IfExistCondition(instance, { type: 'ExpressionStatement', expression: { type: 'Literal', value: 'strict' } }, function() { run = true }).process()
+      expect(run).toBe(false)
+    });
+
+    test("calls function if there is a matching node", () => {
+      let run = false
+      new IfExistCondition(instance, { type: 'ExpressionStatement', expression: { type: 'Literal', value: 'use strict' } }, function() { run = true }).process()
+      expect(run).toBe(true)
+    });
+  });
+});
