1c3a94818f99a6a476fbaa3f30cc2d7c206e9bad
Richard Huang <flyerhzm@gmail.com>
2021-08-26 15:32:21 +0800

    add unlessExistNode dsl

--- test/condition.spec.js
+++ test/condition.spec.js
@@ -1,6 +1,6 @@
 const espree = require("espree");
 
-const { IfExistCondition } = require("../lib/condition");
+const { IfExistCondition, UnlessExistCondition } = require("../lib/condition");
 const Instance = require("../lib/instance");
 
 describe("IfExistsCondition", () => {
@@ -27,6 +27,34 @@ describe("IfExistsCondition", () => {
       let run = false
       new IfExistCondition(instance, { type: 'ExpressionStatement', expression: { type: 'Literal', value: 'use strict' } }, function() { run = true }).process()
       expect(run).toBe(true)
+    });
+  });
+});
+
+describe("UnlessExistsCondition", () => {
+  const source = `
+    'use strict'
+
+    this.foobar
+  `
+  const node = espree.parse(source, { ecmaVersion: 'latest', loc: true, sourceFile: 'code.js' });
+  const instance = new Instance({}, '', function() {})
+
+  describe("process", () => {
+    beforeAll(() => {
+      instance.currentNode = node
+    })
+
+    test("does not call function if no matching node", () => {
+      let run = false
+      new UnlessExistCondition(instance, { type: 'ExpressionStatement', expression: { type: 'Literal', value: 'strict' } }, function() { run = true }).process()
+      expect(run).toBe(true)
+    });
+
+    test("calls function if there is a matching node", () => {
+      let run = false
+      new UnlessExistCondition(instance, { type: 'ExpressionStatement', expression: { type: 'Literal', value: 'use strict' } }, function() { run = true }).process()
+      expect(run).toBe(false)
     });
   });
 });
