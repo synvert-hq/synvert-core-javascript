e783480d33697c5ab782fd2e5b13a6c4695a135e
Richard Huang <flyerhzm@gmail.com>
2021-08-28 12:58:05 +0800

    add gotoNode dsl

--- test/scope.spec.js
+++ test/scope.spec.js
@@ -1,6 +1,6 @@
 const espree = require("espree");
 
-const { WithinScope } = require("../lib/scope");
+const { WithinScope, GotoScope } = require("../lib/scope");
 const Instance = require("../lib/instance");
 
 const parse = (code) => espree.parse(code, { ecmaVersion: 'latest', loc: true, sourceFile: 'code.js' }).body[0];
@@ -24,6 +24,30 @@ describe("WithinScope", () => {
     test("calls function if there is a matching node", () => {
       let run = false
       new WithinScope(instance, { type: 'ClassDeclaration', id: { name: 'FooBar' } }, function() { run = true }).process()
+      expect(run).toBe(true)
+    });
+  });
+});
+
+describe("GotoScope", () => {
+  const source = `class FooBar {}`
+  const node = parse(source)
+  const instance = new Instance({}, '', function() {})
+
+  describe("process", () => {
+    beforeAll(() => {
+      instance.currentNode = node
+    })
+
+    test("does not call function if no matching node", () => {
+      let run = false
+      new GotoScope(instance, 'name', function() { run = true }).process()
+      expect(run).toBe(false)
+    });
+
+    test("calls function if there is a matching node", () => {
+      let run = false
+      new GotoScope(instance, 'id', function() { run = true }).process()
       expect(run).toBe(true)
     });
   });
