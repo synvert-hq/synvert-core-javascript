038aff4e453f865588d186ba6c7dd1f00446607a
Richard Huang <flyerhzm@gmail.com>
2021-08-30 12:32:00 +0800

    add Configuration

---
+++ test/configuration.spec.js
@@ -0,0 +1,15 @@
+const Configuration = require("../lib/configuration");
+
+describe("Configuration", () => {
+  it("path", () => {
+    expect(Configuration.path).toBe(".");
+    Configuration.path = "test";
+    expect(Configuration.path).toBe("test");
+  });
+
+  it("skipFiles", () => {
+    expect(Configuration.skipFiles).toEqual([]);
+    Configuration.skipFiles = ["foo", "bar"];
+    expect(Configuration.skipFiles).toEqual(["foo", "bar"]);
+  });
+});
