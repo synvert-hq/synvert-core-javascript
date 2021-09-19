c720c593486e90f8db96cd28cf7ed0759ab11b04
Richard Huang <flyerhzm@gmail.com>
2021-09-19 08:00:20 +0800

    rewrite NpmVersion

--- test/npm-version.spec.js
+++ test/npm-version.spec.js
@@ -1,13 +1,13 @@
 const NpmVersion = require("../lib/npm-version");
 
 describe("npm version", () => {
-  test("compares versions >=", async () => {
+  test("compares versions >=", () => {
     const npmVersion = new NpmVersion("compare-versions", ">= 3.6.0");
-    expect(await npmVersion.match()).toBe(true);
+    expect(npmVersion.match()).toBe(true);
   });
 
-  test("compares versions <", async () => {
+  test("compares versions <", () => {
     const npmVersion = new NpmVersion("compare-versions", "< 3.6.0");
-    expect(await npmVersion.match()).toBe(false);
+    expect(npmVersion.match()).toBe(false);
   });
 });
