9e22b86bb33eed90c9b3f28adcfcc3493841bafe
Richard Huang <flyerhzm@gmail.com>
2022-01-01 21:16:58 +0800

    add Array#toSource()

--- test/array-ext.spec.js
+++ test/array-ext.spec.js
@@ -1,4 +1,11 @@
+const espree = require("espree");
+const mock = require("mock-fs");
+
+require("../lib/ast-node-ext");
 require("../lib/array-ext");
+
+const parse = (code) =>
+  espree.parse(code, { ecmaVersion: "latest", loc: true, sourceType: "module", sourceFile: "code.js" }).body[0];
 
 describe("array", () => {
   test("first", () => {
@@ -9,5 +16,19 @@ describe("array", () => {
   test("last", () => {
     const array = [0, 1, 2];
     expect(array.last()).toBe(2);
+  });
+
+  test("toSource", () => {
+    const code = `
+      class Foobar {
+        async foobar(foo, bar) {
+        }
+      }
+    `;
+    mock({ "code.js": code });
+    const node = parse(code);
+    const array = node.body.body;
+    expect(array.toSource()).toBe(`        async foobar(foo, bar) {\n        }`);
+    mock.restore();
   });
 });
