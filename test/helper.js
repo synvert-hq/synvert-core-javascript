152fce25883ae08a58053891329c1d219f5cd707
Awesome Code <team@xinminlabs.com>
2022-05-19 08:05:22 +0000

    Auto corrected by following Format Javascript Code

--- test/helper.js
+++ test/helper.js
@@ -4,11 +4,11 @@ require("../lib/ast-node-ext");
 require("../lib/array-ext");
 
 const parse = (code, { firstStatement } = { firstStatement: true }) => {
-  const node = espree.parse(code, { ecmaVersion: "latest", loc: true, sourceType: 'module', sourceFile: "code.js" })
+  const node = espree.parse(code, { ecmaVersion: "latest", loc: true, sourceType: "module", sourceFile: "code.js" });
   if (firstStatement) {
     return node.body[0];
   }
-  return node
-}
+  return node;
+};
 
 module.exports = { parse };
