90ce2ca0bcf96d87f31fbc45793f6ecd82ee3f32
Richard Huang <flyerhzm@gmail.com>
2022-06-22 20:11:59 +0800

    rename ALL_FILES to ALL_JS_FILES and add ALL_TS_FILES

--- src/synvert.ts
+++ src/synvert.ts
@@ -1,9 +1,10 @@
 import Rewriter from "./rewriter";
 import Configuration from "./configuration";
 
-const ALL_FILES = "**/*.{js,jsx}";
+const ALL_JS_FILES = "**/*.{js,jsx}";
+const ALL_TS_FILES = "**/*.{ts,tsx}";
 
 const pjson = require("../package.json");
 const version = pjson.version;
 
-export { Rewriter, Configuration, ALL_FILES, version };
+export { Rewriter, Configuration, ALL_JS_FILES, ALL_TS_FILES, version };
