3b6d071311921275a3242f0ecd46b51c0b178c66
Awesome Code <team@xinminlabs.com>
2022-08-20 05:59:39 +0000

    Auto corrected by following Format Typescript Code

--- src/synvert.ts
+++ src/synvert.ts
@@ -8,4 +8,11 @@ const ALL_TS_FILES = "**/*.{ts,tsx}";
 const pjson = require("../package.json");
 const version = pjson.version;
 
-export { Rewriter, Configuration, ALL_FILES, ALL_JS_FILES, ALL_TS_FILES, version };
+export {
+  Rewriter,
+  Configuration,
+  ALL_FILES,
+  ALL_JS_FILES,
+  ALL_TS_FILES,
+  version,
+};
