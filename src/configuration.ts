d6edbcc0c09c78e54c55ede7a332db074360d047
Richard Huang <flyerhzm@gmail.com>
2024-02-18 20:15:54 +0800

    add Configuration.respectGitignore

--- src/configuration.ts
+++ src/configuration.ts
@@ -5,6 +5,7 @@ class Configuration {
   static onlyPaths: string[] = [];
   static skipPaths: string[] = ["**/node_modules/**"];
   static rootPath: string = ".";
+  static respectGitignore: boolean = true;
   static showRunProcess: boolean = false;
   static maxFileSize: number = 10 * 1024; // 10K
   static singleQuote = false;
